d81e9ffafae7a81f0f64b1a3d48b08d8c7ac2c4a
Stefan Pantic <stefanpantic13@gmail.com>
2018-11-03 14:49:24 +0100

    zavrsen intro

---
+++ src/intro.py
@@ -0,0 +1,69 @@
+""" def saberi(a, b):
+    print(a)
+    print(b)
+    return a+b
+
+def nadovezi_string(s1="s1 ", s2="s2 "):
+    return s1 + s2
+
+def f(l):
+    l = [6,7,8,9]
+    l[0]=12343412
+    l.append(5)
+
+a = 5
+s = "Cao svete!"
+l = [1,2,3,4]
+f(l)
+print(l)
+
+b : int = 17
+
+#br1 = int(input("Unesi jedan broj: "))
+#br2 = float(input("Unesi drugio broj: "))
+
+#print(f'Zbir je {saberi(b=br1, a=br2)}')
+print(nadovezi_string(s2 = "string"))
+
+
+
+
+c  = a + 4
+d = a**c
+
+niz = [1,2,3,4,5,6,7\
+    ,4,56,76]
+ """
+
+""" if a > 10:
+    print(f"{a} je vece od 4")
+elif b < 20:
+    print(f"{b} je manje od 20")
+else:
+    print("nista nije proslo") """
+
+
+slova = ['a', 'b', 'v', 'h']
+"""
+komentar
+"""
+#i ovo je komentar
+#for slovo in slova:
+#    print(slovo, end=' ')
+
+#for i in range(10,0,-2):
+#    print(i)
+
+""" i = 0
+
+while i < 10:
+    i+=1
+    print(i)
+ """
+
+recnik = {'a':5,'b':78}
+print(recnik['a'])
+recnik['a'] = 17
+recnik['c'] = 67
+
+
